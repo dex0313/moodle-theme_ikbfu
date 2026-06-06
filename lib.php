210d60c94a9904783430259f87bb3e9c75566568
Dmitry Kharchuk <temere@gmail.com>
2026-06-06 11:16:08 +0200

    Add file serving functionality for theme settings and refactor SCSS structure

--- lib.php
+++ lib.php
@@ -91,6 +91,32 @@ function theme_ikbfu_get_extra_scss($theme) {
     return !empty($theme->settings->scss) ? "{$theme->settings->scss}  \n  {$content}" : $content;
 }
 
+/**
+ * Serves any files associated with the theme settings.
+ *
+ * @param stdClass $course
+ * @param stdClass $cm
+ * @param context $context
+ * @param string $filearea
+ * @param array $args
+ * @param bool $forcedownload
+ * @param array $options
+ * @return bool
+ */
+function theme_ikbfu_pluginfile($course, $cm, $context, $filearea, $args, $forcedownload, array $options = array()) {
+    if ($context->contextlevel == CONTEXT_SYSTEM && ($filearea === 'logo' || $filearea === 'backgroundimage' ||
+        $filearea === 'loginbackgroundimage')) {
+        $theme = theme_config::load('ikbfu');
+        // By default, theme files must be cache-able by both browsers and proxies.
+        if (!array_key_exists('cacheability', $options)) {
+            $options['cacheability'] = 'public';
+        }
+        return $theme->setting_file_serve($filearea, $args, $forcedownload, $options);
+    } else {
+        send_file_not_found();
+    }
+}
+
 // function theme_ikbfu2021_get_main_scss_content($theme) {
 //     global $CFG;
 
